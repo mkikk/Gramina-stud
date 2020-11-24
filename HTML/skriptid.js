a945c12cb44e00564f930f424b95aee002c44eb9
rometv <romet.vinnal@gmail.com>
2020-11-24 20:12:14 +0200

    Nupu js muutis kodu, stables uuendus, kontaktilehe fail.

---
+++ HTML/skriptid.js
@@ -0,0 +1,12 @@
+window.onclick = function(event) {
+    if (event.target === document.getElementById("nupp")) {
+    document.getElementById("navbar").style.display = "block";
+}
+    else {
+    if (document.getElementById("bar").contains(event.target)){
+}
+    else {
+    document.getElementById("navbar").style.display = "none";
+}
+}
+}
