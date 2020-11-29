66b331710fc1b4db2c26fd7da9d4275780549bda
mkikk <72610081+mkikk@users.noreply.github.com>
2020-11-29 21:22:06 +0200

    Maps, peaaegu korras horses ja staff

--- HTML/skriptid.js
+++ HTML/skriptid.js
@@ -13,5 +13,18 @@ window.onclick = function(event) {
     }
 }
 
-
-
+//google maps
+function initMap() {
+    // Muuda koordinaadid õigeks
+    const gstud = { lat: 58.326036, lng: 26.647291 };
+     
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: gstud,
+    });
+    
+    const marker = new google.maps.Marker({
+      position: gstud,
+      map: map,
+    });
+  }
